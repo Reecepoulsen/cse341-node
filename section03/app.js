42b792bfcb145cec1414c280df5fb29f9de6972e
Reecepoulsen <dreecepoulsen@gmail.com>
2022-01-19 21:24:19 -0700

    added section03's reading assignment

--- section03/app.js
+++ section03/app.js
@@ -7,20 +7,18 @@ const errorController = require('./controllers/error');
 
 const app = express();
 
-// EJS Engine
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000)
-
+app.listen(3000);
--- section03/routes/shop.js
+++ section03/routes/shop.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require('path');
 
 const express = require('express');
 
@@ -10,7 +10,13 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+router.get('/products/:productId', shopController.getProduct);
+
 router.get('/cart', shopController.getCart);
+
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
+router.post('/cart', shopController.postCart)
 
 router.get('/orders', shopController.getOrders);
 
