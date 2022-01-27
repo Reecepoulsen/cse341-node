5e9159068832b4e83326fa99de0c452a0e3a9be3
Reecepoulsen <dreecepoulsen@gmail.com>
2022-01-27 10:55:41 -0700

    messing with mongo url

--- section04/app.js
+++ section04/app.js
@@ -39,16 +39,12 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 const corsOptions = {
-  origin: "https://<your_app_name>.herokuapp.com/",
+  origin: "https://reeces-online-store.herokuapp.com/",
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 
 const options = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
   family: 4,
 };
 
