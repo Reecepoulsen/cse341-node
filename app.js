31038aca15ad82d02a9e69a2c753144a3f6447f3
Reecepoulsen <dreecepoulsen@gmail.com>
2022-02-03 12:01:12 -0700

    added authentication and email sending

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
-require('dotenv').config();
+require("dotenv").config();
 
 const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ophu4.mongodb.net/shop?&w=majority`;
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
+const csrfProtection = csrf();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(
   bodyParser.urlencoded({
@@ -25,9 +37,22 @@ app.use(
   })
 );
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
-  User.findById("61f1d172aa237171ba332702")
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then((user) => {
       req.user = user;
       next();
@@ -35,8 +60,15 @@ app.use((req, res, next) => {
     .catch((err) => console.log(err));
 });
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+})
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
@@ -50,25 +82,11 @@ const options = {
   family: 4,
 };
 
-const MONGODB_URL =
-  process.env.MONGODB_URL ||
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ophu4.mongodb.net/shop?retryWrites=true&w=majority`;
+const MONGODB_URL = process.env.MONGODB_URL || MONGODB_URI;
 
 mongoose
   .connect(MONGODB_URL, options)
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Reece",
-          email: "email@email.com",
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-    });
     app.listen(PORT);
   })
   .catch((err) => {
